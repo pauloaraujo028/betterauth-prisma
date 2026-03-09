83d295f8335895cae965573ad5576fbdfbe868c5
Paulo Araujo <pauloaraujo.phz@gmail.com>
2026-03-09 16:22:52 -0300

    chore: troca de fonte no layout

--- src/features/workspaces/components/workspace-switcher.tsx
+++ src/features/workspaces/components/workspace-switcher.tsx
@@ -17,7 +17,6 @@ const WorkspaceSwitcher = () => {
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
         <p className="text-xs text-neutral-500 uppercase">Workspaces</p>
-        {/* <CirclePlus className="size-5 cursor-pointer text-neutral-500 transition hover:opacity-75" /> */}
         <RiAddCircleFill className="size-5 cursor-pointer text-neutral-500 transition hover:opacity-75" />
       </div>
 
